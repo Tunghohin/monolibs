d22926229c61b238c76352f5cc0c4fb872e65288
Tunghohin <houhin0809@gmail.com>
2024-01-26 22:41:31 +0800

    Remove build

--- build/monolibs/CMakeFiles/monolibs.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libmonolibs.pdb"
-  "libmonolibs.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/monolibs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
