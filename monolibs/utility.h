f9942b9223809f3ac1e010e1b45f6da3ed1a5a6e
Tunghohin <houhin0809@gmail.com>
2024-01-28 19:47:22 +0800

    Update utility

--- monolibs/utility.h
+++ monolibs/utility.h
@@ -2,21 +2,25 @@
 #define __MONOLIBS_UTILITY_H
 
 #include <array>
+#include <chrono>
 #include <fmt/base.h>
 #include <type_traits>
 
 namespace mono {
 
-template <typename Arg0, typename... Args>
-constexpr auto sum(Arg0 arg0, Args... args) -> Arg0 {
-    if constexpr (sizeof...(args) == 0)
-        return arg0;
-    else
-        return arg0 + sum(args...);
+template <typename Arg>
+constexpr auto sum(Arg arg) -> Arg {
+    return arg;
 }
 
 template <typename Arg0, typename... Args>
-inline constexpr auto display(Arg0&& arg0, Args&&... args) -> void {
+    requires std::is_same_v<Arg0, std::common_type_t<Args...>>
+constexpr auto sum(Arg0 arg0, Args... args) -> std::common_type_t<Args...> {
+    return arg0 + sum(args...);
+}
+
+template <typename Arg0, typename... Args>
+constexpr auto display(Arg0&& arg0, Args&&... args) -> void {
     fmt::println("{}", arg0);
     if constexpr (sizeof...(args))
         display(std::forward<Args>(args)...);
@@ -32,6 +36,26 @@ constexpr auto get_array(Args... args)
     -> std::array<std::common_type_t<Args...>, sizeof...(Args)> {
     using T = std::common_type_t<Args...>;
     return std::array<T, sizeof...(Args)>{args...};
+}
+
+template <typename Fn>
+auto invoke_and_time(Fn f) -> decltype(f()) {
+    auto start_time = std::chrono::steady_clock::now();
+    if constexpr (std::is_same_v<decltype(f()), void>) {
+        f();
+        auto end_time = std::chrono::steady_clock::now();
+        auto duration =
+            std::chrono::duration<double, std::milli>(end_time - start_time);
+        fmt::println("{}", duration.count());
+        return;
+    } else {
+        auto ret = f();
+        auto end_time = std::chrono::steady_clock::now();
+        auto duration =
+            std::chrono::duration<double, std::milli>(end_time - start_time);
+        fmt::println("{}", duration.count());
+        return ret;
+    }
 }
 
 } // namespace mono
--- test.cpp
+++ test.cpp
@@ -1,7 +1,24 @@
+#include <chrono>
 #include <fmt/base.h>
 #include <functional.h>
+#include <iostream>
+#include <thread>
+#include <type_traits>
 #include <utility.h>
 
-int arr[mono::sum(1, 2, 3, 3)];
+void func() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(123));
+    return;
+}
 
-auto main() -> int { return 0; }
+int ret_int() {
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(233));
+    return 233;
+}
+
+auto main() -> int {
+    mono::invoke_and_time(func);
+    auto ret = mono::invoke_and_time(ret_int);
+    return 0;
+}
