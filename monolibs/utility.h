1c5f12fcd3495817d42112cb26e4a581f88731da
Tunghohin <houhin0809@gmail.com>
2024-01-29 10:10:01 +0800

    Add compile time fibonacci sequence

--- monolibs/utility.h
+++ monolibs/utility.h
@@ -4,7 +4,13 @@
 #include <array>
 #include <chrono>
 #include <fmt/base.h>
+#include <string>
 #include <type_traits>
+#include <typeinfo>
+#if (defined(__GNUC__) || defined(__clang__)) && __has_include(<cxxabi.h>)
+#include <cstdlib>
+#include <cxxabi.h>
+#endif
 
 namespace mono {
 
@@ -74,6 +80,38 @@ template <>
 struct fibonacci<2> {
     static constexpr int value = 1;
 };
+
+// demangle type type
+static std::string cppdemangle(const char* name) {
+#if (defined(__GNUC__) || defined(__clang__)) && __has_include(<cxxabi.h>)
+    int status;
+    char* p = abi::__cxa_demangle(name, 0, 0, &status);
+    std::string s = p ? p : name;
+    std::free(p);
+#else
+    std::string s = name;
+#endif
+    return s;
+}
+
+static std::string cppdemangle(std::type_info const& type) {
+    return cppdemangle(type.name());
+}
+
+template <class T>
+static std::string cppdemangle() {
+    std::string s{
+        cppdemangle(typeid(std::remove_cv_t<std::remove_reference_t<T>>))};
+    if (std::is_const_v<std::remove_reference_t<T>>)
+        s += " const";
+    if (std::is_volatile_v<std::remove_reference_t<T>>)
+        s += " volatile";
+    if (std::is_lvalue_reference_v<T>)
+        s += " &";
+    if (std::is_rvalue_reference_v<T>)
+        s += " &&";
+    return s;
+}
 
 } // namespace mono
 
