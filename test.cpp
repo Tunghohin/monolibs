d7bc8ed9f9004b79c4c2a06b6a6a1c0ff49a93d2
Tunghohin <houhin0809@gmail.com>
2024-02-12 22:25:55 +0800

    Update submodules

--- test.cpp
+++ test.cpp
@@ -1,19 +1,13 @@
 #include <algorithm>
-#include <fmt/base.h>
 #include <linear_algebra.h>
 #include <numeric>
+#include <spdlog/fmt/bundled/core.h>
+#include <spdlog/spdlog.h>
 #include <type_traits>
 #include <utility>
 #include <vector>
 
 auto main() -> int {
-    mono::matrix<float, 3, 4> m;
-    m[3][4] = 4;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
-            fmt::print("{} ", m[i][j]);
-        }
-        fmt::println("");
-    }
+    fmt::print("{}\n", 233);
     return 0;
 }
